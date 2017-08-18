a07437c8c862c4699c15b902593f4a0790b9b5c8
tchikuba <chikuba.tsutomu@gmail.com>
2017-08-18 19:16:22 +0900

    独自バリデーションを追加

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,4 +1,14 @@
 class Diary < ApplicationRecord
   validates :title, presence: true, length: { maximum: 255 }
   validates :body, presence: true, length: { maximum: 10_000 }
+  validate :proper_title_and_body
+
+  def proper_title_and_body
+    unless title.match(/^今日/) 
+      errors.add(:title, 'は「今日」から始めてください。')
+    end
+    unless body.match(/。$/)
+      errors.add(:body, 'は句点（。）で終了してください。')
+    end
+  end
 end
