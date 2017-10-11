412fe404f3caab6cf7e1dbdb6f11b1305005e181
tchikuba <chikuba.tsutomu@gmail.com>
2017-10-11 17:32:25 +0900

    8章本脱稿指摘を反映

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -3,12 +3,14 @@ class Diary < ApplicationRecord
   validates :body, presence: true, length: { maximum: 10_000 }
   validate :proper_title_and_body
 
-  def proper_title_and_body
-    unless title.match(/^今日/) 
-      errors.add(:title, 'は「今日」から始めてください。')
+  private
+
+    def proper_title_and_body
+      unless title.starts_with?('今日')
+        errors.add(:title, 'は「今日」から始めてください。')
+      end
+      unless body.ends_with?('。')
+        errors.add(:body, 'は句点（。）で終了してください。')
+      end
     end
-    unless body.match(/。$/)
-      errors.add(:body, 'は句点（。）で終了してください。')
-    end
-  end
 end
